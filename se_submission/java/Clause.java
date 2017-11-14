d47f3f7f78e5021b47947afeed2f50edd248c512
varunmaddali <31964126+varunmaddali@users.noreply.github.com>
2017-11-14 00:20:26 -0600

    testing for unit_testing assignment

---
+++ se_submission/java/Clause.java
@@ -0,0 +1,40 @@
+import java.io.*;
+//package iProlog;
+/**
+ * representation of a clause
+ */
+class Clause {
+  Clause(final int len, final int[] hgs, final int base, final int neck, final int[] xs) {
+    this.hgs = hgs; // head+goals pointing to cells in cs
+    this.base = base; // heap where this starts
+    this.len = len; // length of heap slice
+    this.neck = neck; // first after the end of the head
+    this.xs = xs; // indexables in head
+  }
+
+  final int len;
+  final int[] hgs;
+  final int base;
+  final int neck;
+  final int[] xs;
+void print_values()
+{
+System.out.println("the values are len:"+len);
+for(int i=0;i<=len-1;i++)
+System.out.println("the values are hgs:"+hgs[i]);
+System.out.println("the values are base:"+base);
+System.out.println("the values are neck:"+neck);
+for(int i=0;i<=len-1;i++)
+System.out.println("the values are xs:"+xs[i]);
+
+
+}
+
+public static void main(String[] args)
+{
+ int r[]={2,3,4,5,6};
+ Clause c=new Clause(5,r,1,7,r);
+ c.print_values();
+}
+}
+
