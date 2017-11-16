68372a8b8f315a523f0f4f40998aa07f3aae94a4
varunmaddali <31964126+varunmaddali@users.noreply.github.com>
2017-11-16 11:07:58 -0600

    changed headers
    added the count of each type

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.io.*;
  
 class Palindrome
 {
    public static void main(String args[])
    {
       String original, reverse = "";
-      Scanner in = new Scanner(System.in);
- 
-      System.out.println("Enter a string to check if it is a palindrome");
-      original = in.nextLine();
+	int palindrome=0,notpalindrome=0;
+	
+  BufferedReader br = new BufferedReader(new FileReader("common_words.txt"));
+    String line;
+    while ((line = br.readLine()) != null) {
+
+        original = line;
  
       int length = original.length();
  
@@ -16,9 +24,18 @@ class Palindrome
          reverse = reverse + original.charAt(i);
  
       if (original.equals(reverse))
-         System.out.println("Entered string is a palindrome.");
+	{
+	palindrome++;
+         System.out.println("The string -----"+original+"-----is a palindrome.");
+	}
       else
-         System.out.println("Entered string is not a palindrome.");
+	{
+         //System.out.println("The string -----"+original+"-----is not a palindrome.");
+	notpalindrome++;
+	}
+    }  
+	System.out.println("The count of palindromes inn the list is :"+palindrome);
+	System.out.println("The count of palindromes inn the list is :"+notpalindrome);   
  
    }
 }
