6e97365f07f34ab8d52033e2f0cef7135176bf3a
pavankumar <manas@dynamic-10-125-223-107.dynamic.unt.edu>
2017-11-16 10:50:30 -0600

    final

--- pavan exam/test1.java
+++ pavan exam/test1.java
@@ -1,68 +1,57 @@
 package setest1;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
- 
-class test1
-{
-   public static void main(String args[])
-   {
-      String original, reverse = "";
-      Scanner in = new Scanner(System.in);
- 
-      System.out.println("Enter a string to check if it is a palindrome");
-      original = in.nextLine();
-      
-      
-      String fileName = "temp.txt";
+import java.io.*;
 
-      // This will reference one line at a time
-      String line = null;
 
-      try {
-          // FileReader reads text files in the default encoding.
-          FileReader fileReader = 
-              new FileReader(fileName);
+public class test1 {
+        public static void main(String args[]) throws IOException
+        {
+            
+            String fileName = "Downloads\\palindrome.txt";//name of the string
 
-          // Always wrap FileReader in BufferedReader.
-          BufferedReader bufferedReader = 
-              new BufferedReader(fileReader);
+            
+            String line = null;
 
-          while((line = bufferedReader.readLine()) != null) {
-              System.out.println(line);
-          }   
+            try {
+                
+                FileReader fileReader =
+                        new FileReader(fileName);
 
-          // Always close files.
-          bufferedReader.close();         
-      }
-      catch(FileNotFoundException ex) {
-          System.out.println(
-              "Unable to open file '" + 
-              fileName + "'");                
-      }
-      catch(IOException ex) {
-          System.out.println(
-              "Error reading file '" 
-              + fileName + "'");                  
-          // Or we could just do this: 
-          // ex.printStackTrace();
-      }
-  }
+               
+                BufferedReader bufferedReader =
+                        new BufferedReader(fileReader);
 
-      
- 
-      int length = original.length();
- 
-      for ( int i = length - 1; i >= 0; i-- )
-         reverse = reverse + original.charAt(i);
- 
-      if (original.equals(reverse))
-         System.out.println("Entered string is a palindrome.");
-      else
-         System.out.println("Entered string is not a palindrome.");
- 
-   }
-}
+                while((line = bufferedReader.readLine()) != null) {
+                    String original, reverse = "";
+
+                    original = line;
+                    System.out.print(line);
+                    System.out.print("----**---");
+                    int length = original.length();
+
+                    for ( int i = length - 1; i >= 0; i-- )
+                        reverse = reverse + original.charAt(i);//checking here
+
+                    if (original.equals(reverse))
+                        System.out.println(" YES it is palindrome");
+                    else
+                        System.out.println("NO it not palindrome");
+
+                }
+                bufferedReader.close();
+                }
+            catch(FileNotFoundException ex) {
+                System.out.println(
+                        "Unable to open file '" +
+                                fileName + "'");
+            }
+
+                }
+
+                // Always close files.
+
+            }
+
+
+
+
