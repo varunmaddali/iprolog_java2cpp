4043089bafb7cd4e2ae12efebeaa9ea82df915c4
arjun2694 <32027735+arjun2694@users.noreply.github.com>
2017-12-03 07:07:41 -0600

    Add files via upload

---
+++ iprolog_cpp/src/Object.h
@@ -0,0 +1,78 @@
+#ifndef __OBJECT_H__
+#define __OBJECT_H__
+
+#include <vector>
+#include <sstream>
+
+
+/**
+*	This Class can hold a single value. It's either a std::string, an int, or a std::vector<Object>
+*/
+
+class Object
+{
+public:
+	static const int T_NULL = 0;
+	static const int T_STRING = 1;
+	static const int T_INT = 2;
+	static const int T_ARRAY = 3;
+
+
+	Object()
+		: sval("NULL"), type(T_NULL)
+	{
+	}
+
+	Object(std::string s)
+		: sval(s), type(T_STRING)
+	{
+	}
+
+	Object(int i)
+		: ival(i), type(T_INT)
+	{
+
+	}
+
+	Object(std::vector<Object>& v)
+		: vval(v), type(T_ARRAY)
+	{
+
+	}
+
+	std::string toString() const
+	{
+		if (type == T_NULL)
+			return sval;
+		if (type == T_STRING)
+			return sval;
+		if (type == T_INT)
+			return std::to_string(ival);
+
+		// vector
+		std::ostringstream os;
+		os << "[";
+		for (unsigned int i = 0 ; i < vval.size() ; i++)
+		{
+			if (i > 0)
+				os << ", ";
+			os << vval[i].toString();
+		}
+		os << "]";
+		return os.str();
+	}
+
+	bool isNull() const
+	{
+		return type == T_NULL;
+	}
+
+
+private:
+	std::vector<Object> vval;
+	int ival;
+	std::string sval;
+	int type;
+};
+
+#endif
