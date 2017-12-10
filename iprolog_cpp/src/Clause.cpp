372e216ee42f3063c28bc167049e58b7c7215985
arjun2694 <32027735+arjun2694@users.noreply.github.com>
2017-12-10 07:09:46 -0600

    added comments

--- iprolog_cpp/src/Clause.cpp
+++ iprolog_cpp/src/Clause.cpp
@@ -1,18 +1,18 @@
 #include "Clause.h"
 
-
-
+//declaring a constructor 
 Clause::Clause::Clause(const int len, const std::vector<int>& hgs, const int base, const int neck, const std::vector<int>& xs)
 	: hgs(hgs), base(base), len(len), neck(neck), xs(xs)
 {
 }
 
-
+//destructor to take care of the completed objects
 Clause::~Clause()
 {
 }
 
 #include <sstream>
+//clause class that gets all the values
 std::string Clause::toString()
 {
 	std::ostringstream os;
